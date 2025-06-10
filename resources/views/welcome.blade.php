0154272ce964d96a31fcee7aba53298d8926a4d8
Leonel Lopez <technoquo@gmail.com>
2025-06-10 14:45:22 +0200

    ajustes de home y general

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,15 +6,17 @@
     <title>Nous construisons notre site web</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="bg-gray-100 text-gray-800 font-sans antialiased min-h-screen flex items-center justify-center">
-<div class="bg-white rounded-lg shadow p-8 max-w-lg w-full text-center">
-    <img src="{{ asset('img/logo_cfls.png') }}" alt="Construction" class="mx-auto mb-6">
-    <h1 class="text-3xl font-bold mb-4">Nous construisons notre site web !</h1>
-    <p class="text-gray-600 mb-6">
+<body class="bg-gray-100 text-gray-800 font-sans antialiased min-h-screen flex items-center justify-center px-4">
+<div class="bg-white rounded-lg shadow p-6 sm:p-8 max-w-lg w-full text-center">
+    <img src="{{ asset('img/logo_cfls.png') }}" alt="Construction" class="mx-auto mb-6 w-32 sm:w-40 md:w-48">
+    <h1 class="text-2xl sm:text-3xl font-bold mb-4">
+        Nous construisons notre site web !
+    </h1>
+    <p class="text-gray-600 mb-6 text-base sm:text-lg">
         Nous travaillons actuellement d'arrache-pied pour lancer notre site très bientôt.
         Revenez nous voir dans quelques jours pour découvrir toutes les nouveautés que nous préparons.
     </p>
-    <p class="text-gray-600 mb-6">
+    <p class="text-gray-600 mb-6 text-base sm:text-lg">
         En attendant, vous pouvez visiter notre site alternatif :
         <a href="https://wix.cfls.be" class="text-blue-500 hover:underline">wix.clfs.be</a>
     </p>
@@ -24,4 +26,3 @@
 </div>
 </body>
 </html>
-
