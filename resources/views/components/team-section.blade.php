ea25ea63a714d04a50b4da61485b6613945c5d67
Leonel Lopez <technoquo@gmail.com>
2025-06-17 13:47:49 +0200

    changed javascript

--- resources/views/components/team-section.blade.php
+++ resources/views/components/team-section.blade.php
@@ -1,13 +1,25 @@
 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mt-12">
-    @foreach ($teamMembers as $member)
+    @foreach ($teamMembers as $index => $member)
+        @php
+            $images = array_filter([
+
+                $member['image_three'],
+                 $member['image_two'],
+            ]);
+        @endphp
+
         <div class="flex flex-col items-center dark:bg-gray-900 rounded-lg overflow-hidden relative">
             <div>
-                <!-- Imagen principal -->
-                <livewire:rotating-image :images="[
-                    $member['image'],
-                    $member['image_two'],
-                    $member['image_three'],
-                ]"/>
+                <img
+                    id="photo-{{ $index }}"
+                    src="{{ asset('storage/' . $member['image']) }}"
+                    data-main="{{ asset('storage/' . $member['image']) }}"
+                    data-images='@json(array_values($images))'
+                    data-current="0"
+                    class="w-[270px] h-[338px] object-cover rounded cursor-pointer transition-all duration-300"
+                    onmouseenter="startRotation(this)"
+                    onmouseleave="stopRotation(this)"
+                />
             </div>
 
             <div class="p-6 text-center">
@@ -16,4 +28,29 @@
             </div>
         </div>
     @endforeach
+
+
+    @push('scripts')
+            <script>
+                const intervals = {};
+
+                function startRotation(img) {
+                    const images = JSON.parse(img.dataset.images);
+                    if (!images.length) return;
+
+                    let i = 0;
+                    intervals[img.id] = setInterval(() => {
+                        i = (i + 1) % images.length;
+                        img.src = `/storage/${images[i]}`;
+                    }, 500);
+                }
+
+                function stopRotation(img) {
+                    clearInterval(intervals[img.id]);
+                    delete intervals[img.id];
+                    // Restaurar imagen principal
+                    img.src = img.dataset.main;
+                }
+            </script>
+        @endpush
 </div>
