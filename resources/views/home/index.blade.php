427a5508d3a0044500e0fefd5e15484f4701361f
Leonel Lopez <technoquo@gmail.com>
2025-06-25 14:27:33 +0200

    put commander top

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,10 +1,10 @@
 <x-layout>
     <x-slot name="title">Accueil</x-slot>
+    @include('home.sections.commader')
     @include('home.sections.hero')
     @include('home.sections.objectif')
     @include('home.sections.donation')
     @include('home.sections.membre')
-    @include('home.sections.commader')
     @include('home.sections.testimonial')
     @include('home.sections.subscribe')
 </x-layout>
