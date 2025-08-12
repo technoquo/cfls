f6fc6385aece1123acf1d7d345c50298b98c5322
Leonel Lopez <technoquo@gmail.com>
2025-08-12 11:18:26 +0200

    remove temporal

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,10 +1,4 @@
 <x-layout>
     <x-slot name="title">Accueil</x-slot>
-    @include('home.sections.commader')
-    @include('home.sections.hero')
-    @include('home.sections.objectif')
-    @include('home.sections.donation')
-    @include('home.sections.membre')
-    @include('home.sections.testimonial')
-{{--    @include('home.sections.subscribe')--}}
+  
 </x-layout>
