c2b02cef5d6ad146d2afc6f927ffa0e009fe9558
Leonel Lopez <technoquo@gmail.com>
2025-09-08 14:52:29 +0200

    add api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+
+
+
+Route::get('/login', [AuthController::class, 'login']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
