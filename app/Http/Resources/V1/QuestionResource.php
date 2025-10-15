1bb75d6bb56f484347a0e7b8e9bfe2705f513d46
Leonel Lopez <technoquo@gmail.com>
2025-10-15 14:55:45 +0200

    save por temporal questionresource

--- app/Http/Resources/V1/QuestionResource.php
+++ app/Http/Resources/V1/QuestionResource.php
@@ -15,8 +15,15 @@ class QuestionResource extends JsonResource
             'theme'         => $this->themes ?? '',
             'question_text' => $this->question_text ?? '',
             'type'          => $this->type ?? '',
-            'video'         => $this->video->url ?? '', // si tienes relación con video_themes_cloudinary
-            'options'       => $this->options ?? [],
+            'video'         => isset($this->video->url)
+                ? urldecode(pathinfo($this->video->url, PATHINFO_FILENAME))
+                : null,
+            'options' => collect($this->options ?? [])->map(function ($option) {
+                if (isset($option['video'])) {
+                    $option['video'] = urldecode(pathinfo($option['video'], PATHINFO_FILENAME));
+                }
+                return $option;
+            })->toArray(),
             'answer'        => $this->answer ?? '',
         ];
     }
