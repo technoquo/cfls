60e38029b961d8e171277a84f8720aa82fce9202
Leonel Lopez <technoquo@gmail.com>
2026-01-12 14:00:25 +0100

    changed route spell without params

--- app/Http/Controllers/Api/V1/SpellController.php
+++ app/Http/Controllers/Api/V1/SpellController.php
@@ -15,15 +15,21 @@ class SpellController extends Controller
 {
     public function index($syllabu = null, $theme = null)
     {
-        $theme = Theme::where('slug', $theme)->first();
-        $syllabus = Syllabu::where('slug', $syllabu)->first();
+//        $theme = Theme::where('slug', $theme)->first();
+//        $syllabus = Syllabu::where('slug', $syllabu)->first();
 
 
+
+
+//        $words = Word::select('id', 'name', 'video_theme_cloudinary_id')
+//            ->where('theme_id', $theme->id)
+//            ->where('syllabu_id', $syllabus->id)
+//            ->whereActive(true)
+//            ->inRandomOrder()
+//            ->get();
 
 
         $words = Word::select('id', 'name', 'video_theme_cloudinary_id')
-            ->where('theme_id', $theme->id)
-            ->where('syllabu_id', $syllabus->id)
             ->whereActive(true)
             ->inRandomOrder()
             ->get();
